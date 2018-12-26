08d3881cbfb9b56cc16668a10aca4e7afe64d4e9
wangqiang <529261905@qq.com>
2018-12-26 19:56:37 +0800

    同步本地文件

---
+++ keepAccounts_for_manager/compileBefore/config/sit.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+  NODE_ENV: '"test"',
+  ENV_CONFIG: '"sit"',
+  BASE_API: '"https://api-sit"'
+}
---
+++ keepAccounts_for_manager/dao/indexDao.js
@@ -0,0 +1,36 @@
+var db=require("./database.js");
+
+//后台登录
+module.exports.login=function({},func){
+  db.collection("adminUser").find({},func);
+};
+
+//查询用户
+module.exports.findUser=function({},func){
+  db.collection("user").find({},func);
+};
+
+//查询留言
+module.exports.findMessage=function({},func){
+  db.collection("contact").find({},func);
+};
+
+//查询年度账单
+module.exports.findYearMoney=function(year,userId,func){
+  db.collection("account").find({year:year,userId:userId},func);
+};
+
+//查询月度账单
+module.exports.findMonthMoney=function(year,month,userId,func){
+  db.collection("account").find({year:year,month:month,userId:userId},func);
+};
+
+//查询单月分页账单
+module.exports.findMonthByPage=function(curpage,eachpage,option,func){
+  db.collection("account").findByPage(curpage,eachpage,option,func);
+};
+
+//删除账单
+module.exports.delMoney=function(id,func){
+  db.collection("account").remove({_id:db.ObjectID(id)},func);
+};
